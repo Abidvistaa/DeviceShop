83c3679ccffb0ecc88720db55550e68bc3369ca3
Abidvistaa <abidhassandiba@gmail.com>
2021-06-06 11:02:36 +0600

    Section1- ProductType Model Migration

--- DeviceShop/Data/ApplicationDbContext.cs
+++ DeviceShop/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DeviceShop.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace DeviceShop.Data
             : base(options)
         {
         }
+        public DbSet<ProductType> ProductTypes { get; set; }
     }
 }
---
+++ DeviceShop/Data/Migrations/20210606050024_AddProductTypeToDB.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeviceShop.Data.Migrations
+{
+    public partial class AddProductTypeToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductsType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTypes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductTypes");
+        }
+    }
+}
