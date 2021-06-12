1a3d0093c0808f8805e2e802d41d61c51d2c9fe8
Abidvistaa <abidhassandiba@gmail.com>
2021-06-12 23:10:48 +0600

    Section:16 Jquery Validation for ASPNetUser fileds

---
+++ DeviceShop/Areas/Customer/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using DeviceShop.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceShop.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class UserController : Controller
+    {
+        UserManager<IdentityUser> _userManager;
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userManager.CreateAsync(user, user.PasswordHash);
+                if (result.Succeeded)
+                {
+                    //TempData["save"] = "User has been created Successfully";
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+                
+            }
+            
+            return View();
+        }
+    }
+}
