b127fcb401eb863504b982216064cdf65a04458a
Abidvistaa <abidhassandiba@gmail.com>
2021-06-15 20:35:59 +0600

    Section21: Admin Recieved Orders

---
+++ DeviceShop/Areas/Admin/ProductOrderController.cs
@@ -0,0 +1,26 @@
+﻿using DeviceShop.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceShop.Areas.Admin
+{
+    [Authorize(Roles ="Admin")]
+    [Area("Admin")]
+    public class ProductOrderController : Controller
+    {
+        ApplicationDbContext _db;
+        public ProductOrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+
+            return View(_db.Orders.ToList());
+        }
+    }
+}
